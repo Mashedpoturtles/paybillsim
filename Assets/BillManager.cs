e24a0a880b605f07d50b72d256b1e9deb3e81d2f
sai <sirean@protonmail.com>
2016-03-08 10:00:55 +0100

    Quick clean up

--- Assets/BillManager.cs
+++ Assets/BillManager.cs
@@ -4,8 +4,39 @@ using System.Collections;
 public class BillManager : MonoBehaviour {
 
     public static BillManager instance;
- 
- 
+
+    //issue all the bills at their set times in update.
+    void Update()
+    {
+        IssueElectricityBill();
+        IssueInternetBill();
+    }
+
+    void IssueElectricityBill()
+    {
+        if (TimeManager.instance.Day == 1 || TimeManager.instance.Day == 7 || TimeManager.instance.Day == 14 || TimeManager.instance.Day == 21)
+        {
+            // so long as this bill does not already exist it may currently spawn.
+            //TODO bill list.
+            if (ElectricityBill.instance.Exists != true)
+            {
+                ElectricityBill.instance.Electricity();
+            }
+        }
+    }
+
+    void IssueInternetBill()
+    {
+        if (TimeManager.instance.Day == 3 || TimeManager.instance.Day == 9 || TimeManager.instance.Day == 18 || TimeManager.instance.Day == 26)
+        {
+            // so long as this bill does not already exist it may currently spawn.
+            //TODO bill list.
+            if (InternetBill.instance.Exists != true)
+            {
+                InternetBill.instance.Internet();
+            }
+        }
+    }
     void Start()
     {
         instance = this;
