27918fb46b96bf26ceaa684b28f627ce0ec57a65
sai <sirean@protonmail.com>
2016-03-10 12:27:26 +0100

    i broke it

--- Assets/BillSystem/BillManager.cs
+++ Assets/BillSystem/BillManager.cs
@@ -2,57 +2,78 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using Assets.BillSystem;
 
 
-public class BillManager : MonoBehaviour {
+namespace Assets.BillSystem {
+    public class BillManager : MonoBehaviour {
 
-    public GameObject _Holder;
-    public static BillManager instance;
-	public static List<Bill> Bills = new List<Bill>();
+        #region
+        public GameObject _Holder;
+        public static BillManager instance;
+        public static List<Bill> Bills = new List<Bill>();
+        public  Dictionary<DayOfWeek, List<BillType>> IssueDays = new Dictionary<DayOfWeek, List<BillType>>();
+        #endregion
 
-	public void Start()
- 
-    {
-        Application.runInBackground = true;
-        instance = this;
-     TimeManager.OnDayChange += IssueBills; // Subscribe to the OnDayChange event.
-    }
 
-    //issue all the bills at their set times in update.
-    void Update()
-    {
-       
-    }
+        public void Start()
 
-	public void IssueBill(BillType type)
-    {
-		if (!IsTuesday(TimeManager.currentTime.DayOfWeek) || !IsThursday(TimeManager.currentTime.DayOfWeek)) return;
-       
-        Bill bill = _Holder.AddComponent<Bill>();
-	}
-    public void IssueBills()
-    {
-        IssueBill(BillType.Internet);
-        IssueBill(BillType.Electricity); 
-    }
-	public static bool IsTuesday(DayOfWeek day)
-    {
-        return day == DayOfWeek.Tuesday;
-	}
-    public static bool IsThursday(DayOfWeek day)
-    {
-        return day == DayOfWeek.Thursday;
-    }
+        {
+            Application.runInBackground = true;
+            instance = this;
+            TimeManager.OnDayChange += IssueBills; // Subscribe to the OnDayChange event.
+        }
 
-	public static int GetBillCount(BillType type)
-    {
-		return Bills.Select(bill => bill.Type == type).ToList().Count;
-	}
+        //issue all the bills at their set times in update.
+        void Update()
+        {
 
-    public static List<Bill> GetBillsByType(BillType type)
-    {
-        return Bills.Select(bill => bill).ToList();
+        }
+
+        public void IssueBill(BillType type)
+        {
+            if (!IsDay(TimeManager.currentTime.DayOfWeek))
+                return;
+            Bill bill = _Holder.AddComponent<Bill>();
+        }
+
+        public void IssueBills()
+        {
+            IssueBill(BillType.Internet);
+            IssueBill(BillType.Electricity);
+        }
+        public static bool IsDay(DayOfWeek day)
+        {
+            return day == DayOfWeek.Tuesday;
+        }
+
+
+        public void IssueStartingPoint()
+        {
+            AddIssueDay(DayOfWeek.Thursday, List<BillType>; // issue here
+           
+        }
+
+        public void AddIssueDay(DayOfWeek day, List<BillType> type)
+        {
+            IssueDays.Add(day, type);
+            /// issue here 
+        }
+    
+
+        public void RemoveIssueDay(DayOfWeek day)
+        {
+            IssueDays.Remove(DayOfWeek.Tuesday);
+            IssueDays.Remove(DayOfWeek.Thursday);
+        }
+
+        public static int GetBillCount(BillType type)
+        {
+            return Bills.Select(bill => bill.Type == type).ToList().Count;
+        }
+
+        public static List<Bill> GetBillsByType(BillType type)
+        {
+            return Bills.Select(bill => bill).ToList();
+        }
     }
 }
- 
