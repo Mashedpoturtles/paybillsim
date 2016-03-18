b418f1ebe5046a094d781ed817ec28a102d04f69
sai <sirean@protonmail.com>
2016-03-18 09:37:33 +0100

    fixed pay button

--- Assets/BillSystem/BillManager.cs
+++ Assets/BillSystem/BillManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,18 +14,14 @@ namespace Assets.BillSystem
 
         public void CreateBill()
         {
-
             foreach (Bill bill in Billholder)
             {
                 if (!bill.IsShown)
                 {
                     bill.IsShown = true;
 
-
                     string billInformation = "";
-
                     billInformation = "Random";
-
                     billInformation = string.Format("Bill type: {0} \\n Issue date: {1} \\n  Due date: {2} \\n Amount to pay: {3} \\n",
                                       Enum.GetName(typeof(BillType), bill.Type),
                                       bill.IssueDate.ToString("d"),
@@ -36,42 +33,37 @@ namespace Assets.BillSystem
             }
         }
 
-        // this is what puts the UI together
         public void CreateUILogic(string billInformationText)
         {
-            //spawn the ui prefab
             GameObject billInformation = (GameObject)GameObject.Instantiate(Resources.Load("billInfo"));
 
-            //find the text component inside the prefab and assign it the billinformation 
             billInformation.GetComponentInChildren<Text>().text = billInformationText;
 
-            //find the pay button in the prefab by its tag and give it an onlick listener with Paybill() assigned to it
-            Button buttonPay = GameObject.FindWithTag("buttonPay").GetComponent<Button>();
+            Button buttonPay;
+            buttonPay = billInformation.GetComponentInChildren<Button>();
             buttonPay.onClick.AddListener(() => PayBill());
-            // find the return button in the prefab by its tag and give it an onlick listener with ReturnBill() assigned to it
-            Button buttonReturn = GameObject.FindWithTag("buttonReturn").GetComponent<Button>();
+
+            Button buttonReturn;
+            buttonReturn = billInformation.GetComponentInChildren<Button>();
             buttonReturn.onClick.AddListener(() => ReturnBill());
 
-            //position the UI elements by parenting it to the canvas in the scene.
             billInformation.transform.SetParent(canvas.transform, false);
             billInformation.transform.localPosition = Vector3.zero;
         }
 
         void Update()
         {
-            Debug.Log(Billholder.Count); // keep count of the total added bills to list
+            Debug.Log(Billholder.Count);
         }
 
         public void Start()
         {
-            //grabs the Canvas component
             canvas = GameObject.FindWithTag("Canvas").GetComponent<Canvas>();
             Application.runInBackground = true;
 
-            TimeManager.OnDayChange += IssueBill; // this event makes sure that a bill wont issue bills more than once on its assigned day
+            TimeManager.OnDayChange += IssueBill;
         }
 
-        //Create a bill on the set days below and add one to the list
         public void IssueBill()
         {
             switch (TimeManager.currentTime.DayOfWeek)
@@ -89,16 +81,13 @@ namespace Assets.BillSystem
 
         public void PayBill()
         {
-            Debug.Log("You paid!");
-
-            for (int i = 0; i >= Billholder.Count; i++) { /* other logic */ Billholder.RemoveAt(i); }
-            {
-
-
-            }
-
-
             Destroy(GameObject.FindWithTag("billinfo"));
+            foreach (Bill bill in Billholder.ToList())
+            {
+                Debug.Log("You paid!");
+
+                Billholder.Remove(bill);
+            }
         }
 
         public void ReturnBill()
