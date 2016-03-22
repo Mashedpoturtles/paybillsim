aeea3e6eee30e5e476a9cdf5198340c66cecdbcb
sai <sirean@protonmail.com>
2016-03-22 13:10:05 +0100

    Work system currently bugged

--- Assets/Scripts/WorkSystem/Work.cs
+++ Assets/Scripts/WorkSystem/Work.cs
@@ -4,8 +4,10 @@ using UnityEngine.UI;
 
 public class Work : MonoBehaviour
 {
+    private Text workStateText;
     private Slider workSlider;
     private float WorkIntensity;
+    private float WorkEnergy;
 
     private void ChangeWorkState()
     {
@@ -16,71 +18,69 @@ public class Work : MonoBehaviour
     {
         WorkIntensity = workSlider.value;
 
-        if (WorkIntensity <= 0.4f)
+        if (WorkIntensity <= 0.3f)
         {
             StartCoroutine(NoWork());
         }
-        else if (WorkIntensity >= 0.4f && WorkIntensity <= 0.8f)
+        else if (WorkIntensity >= 0.3f && WorkIntensity <= 0.5f)
         {
             StartCoroutine(WorkAverage());
         }
-        else if (WorkIntensity >= 0.8f && WorkIntensity <= 0.9f)
+        else if (WorkIntensity >= 0.5f && WorkIntensity <= 0.8f)
         {
             StartCoroutine(WorkHard());
         }
-        else if (WorkIntensity >= 0.9f)
+        else if (WorkIntensity >= 0.8f)
         {
-            StartCoroutine(OverDrive());
-        }
+            StartCoroutine(OverDrive()); }
+        
     }
 
     private IEnumerator NoWork()
     {
-        Debug.Log("not working");
         while (true)
         {
             yield return new WaitForSeconds(3);
+            workStateText.text = "Working pace: Not working";
             Money.currentMoney += 0;
-            Debug.Log("0 money earned");
+            
         }
     }
 
     private IEnumerator WorkAverage()
     {
-        Debug.Log("Working at average pace");
        while(true)
         {
             yield return new WaitForSeconds(3);
+            workStateText.text = "Working pace: Average";
             Money.currentMoney += 10;
-            Debug.Log("10 money earned");
         }
     }
 
     private IEnumerator WorkHard()
     {
-        Debug.Log("working at a hard pace");
        while(true)
         {
             yield return new WaitForSeconds(3);
+            workStateText.text = "Working pace: Hard";
             Money.currentMoney += 20;
-            Debug.Log("20 money earned");
         }
     }
 
     private IEnumerator OverDrive()
     {
-        Debug.Log("Youre a maniac");
        while(true)
         {
             yield return new WaitForSeconds(3);
+            workStateText.text = "working pace: OverDrive";
             Money.currentMoney += 30;
-            Debug.Log("30 money earned");
         }
     }
 
     private void Start()
     {
         workSlider = GameObject.FindWithTag("WorkSlider").GetComponent<Slider>();
+        workStateText = workSlider.GetComponentInChildren<Text>();
         ChangeWorkState();
     }
 }
