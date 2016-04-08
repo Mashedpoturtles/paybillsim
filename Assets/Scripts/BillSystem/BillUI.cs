26690e153bffe0326c5296d0f44f6dd284567f05
MashedPoturtles <sirean@gmail.com>
2016-04-08 11:26:56 +0200

    Added a seperate script component that handles adding the UI to a bill prefab.

---
+++ Assets/Scripts/BillSystem/BillUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Assets.BillSystem;
+using System;
+
+public class BillUI : MonoBehaviour
+    {
+    [SerializeField]
+    private Text informationTextLabel;
+    [SerializeField]
+    private Button payButton;
+
+    public void SetInfo ( BillManager manager, Bill bill )
+        {
+        payButton.onClick.AddListener ( ( ) => manager.PayBill ( bill ) );
+
+        this.informationTextLabel.text = string.Format ( "Bill type: {0} \\n Issue date: {1} \\n  Due date: {2} \\n Amount to pay: {3} \\n",
+                                    Enum.GetName ( typeof ( BillType ), bill.Type ),
+                                    bill.IssueDate.ToString ( "d" ),
+                                    bill.DueDate.ToString ( "d" ),
+                                    bill.Amount ).Replace ( "\\n", "\n" );
+        // Debug.Log ( "I'm about to pay " + bill.Amount + " and i have " + Money.instance.currentMoney + " my obj is : " + bill.Object, bill.Object );
+        }
+    }
