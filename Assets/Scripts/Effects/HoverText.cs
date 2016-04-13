4c11dc5c3c0edb6f34211ae7204374bbdd38cae3
MashedPoturtles <sirean@gmail.com>
2016-04-13 16:47:31 +0200

    Added glow and sound effects to UI

---
+++ Assets/Scripts/Effects/HoverText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class HoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+    [SerializeField]
+    private Text target;
+
+    private void Start ( )
+        {
+        target.text = "";
+        }
+    public void OnPointerEnter ( PointerEventData eventData )
+        {
+        target.text = "You can drop your card here.";
+        }
+
+    public void OnPointerExit ( PointerEventData eventData )
+        {
+        target.text = "";
+        }
+    }
